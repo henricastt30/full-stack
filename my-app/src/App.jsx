8ff4ebda82968f5df86ef0b95f577a013480c50a
HENRIQUE MOREIRA BARELA <henrique_barela@estudante.sesisenai.org.br>
2025-09-29 21:44:12 -0300

    Feat: Adicionando Nav_Bar

---
+++ my-app/src/App.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import User from './components/User/User'
+import Menu from './components/Menu/Menu'
+
+function App() {
+  return (
+    <>
+      {/* <h1>It's me!</h1> */}
+      {/* <User/> */}
+      <Menu/>
+    </>
+  )
+}
+
+export default App
---
+++ my-app/src/components/User/User.jsx
@@ -0,0 +1,11 @@
+// Componente User
+
+const User = () => {
+    return (
+        <>
+            <div>Usuário!!!!</div>
+        </>
+    )
+}
+
+export default User;
